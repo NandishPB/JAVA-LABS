b58061350805659e9d91eeeda38d68e039a35650
Nandish PB <nandish.22ai026@sode-edu.in>
2023-12-05 22:04:26 +0530

    program which prints our Name by taking input by the user

---
+++ Name.java
@@ -0,0 +1,12 @@
+ import java.util.*;
+ public class Name {
+    public static void main(String[] args){
+        String qq;
+        System.out.println("Enter your Name");
+        Scanner s = new Scanner(System.in);
+        qq  = s.nextLine();
+        System.out.println(qq);
+        s.close();
+    }
+    
+}
